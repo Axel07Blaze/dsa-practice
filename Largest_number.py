4e3cb6c5dcb0033419ab73093cd93046b83d2e85
Axel07Blaze <146859071+Axel07Blaze@users.noreply.github.com>
2026-06-22 17:29:34 +0530

    Add files via upload

---
+++ Largest_number.py
@@ -0,0 +1,29 @@
+# identify largest number
+#among [0,10,40,80,60,50,30]
+
+numbers = [0,10,40,50,30,20,80,70]
+
+largest = numbers[0]
+
+if numbers[0] > largest:
+    largest = numbers[0]
+
+if numbers[1] > largest:
+    largest = numbers[1]
+
+if numbers[3] > largest:
+    largest = numbers[3]
+
+if numbers[4] > largest:
+    largest = numbers[4]
+
+if numbers[5] > largest:
+    largest = numbers[5]    
+
+if numbers[6] > largest:
+    largest = numbers[6]
+
+if numbers[7] > largest:
+    largest = numbers[7]
+
+print("largest:",largest)    
