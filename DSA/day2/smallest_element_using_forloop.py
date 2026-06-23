bebb44994219cfd1b97f26f846327e412048cad3
Axel07Blaze <146859071+Axel07Blaze@users.noreply.github.com>
2026-06-23 17:43:19 +0530

    Create smallest_element_using_forloop.py

---
+++ DSA/day2/smallest_element_using_forloop.py
@@ -0,0 +1,12 @@
+#identify the smallest element in a list using for loop
+numbers = [10,13,20,60,4,11,17]
+smallest= numbers[0]
+for num in numbers:
+    if num < smallest:
+        smallest = num
+
+print (smallest)
+
+
+
+    
