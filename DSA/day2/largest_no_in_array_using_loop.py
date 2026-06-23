81bf71f9217e4131fe7f9a5cb8a5d36868ecbf6f
Axel07Blaze <146859071+Axel07Blaze@users.noreply.github.com>
2026-06-23 17:43:04 +0530

    Create largest_no_in_array_using_loop.py

---
+++ DSA/day2/largest_no_in_array_using_loop.py
@@ -0,0 +1,12 @@
+#find the largest no in array using for loop
+numbers = [10,20,40,30,50,70,92]
+
+largest = numbers[0]
+
+for num in numbers:
+    if num > largest:
+     largest = num
+
+print (largest)
+
+    
